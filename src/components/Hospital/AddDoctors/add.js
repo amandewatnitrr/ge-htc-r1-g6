c84b7f58bcfc1f6bf236582eb62e395a382894f4
Aman Kumar Dewangan <amandewatnitrr@gmail.com>
2022-12-26 00:23:28 +0530

    Add files via upload

--- src/components/Hospital/AddDoctors/add.js
+++ src/components/Hospital/AddDoctors/add.js
@@ -1,15 +1,222 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from '../Navbar/navbar';
 import Footer from '../../Footer/footer';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Card from 'react-bootstrap/Card';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import './css/add.css';
 
-function hospital() {
+function Hospital() {
+
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  };
+
   return (
     <>
+        <div style={{'background': 'linear-gradient(#7F7FD5, #91eae4)' }}>
         <Navbar />
-        <div id='new'>Add Doctors</div>
+        <div style={{'min-height':'50vh'}}>
+        <Card id='SignOutCard'>
+        <Card.Body>
+        <Card.Header>
+        <Card.Title>Add Doctors</Card.Title>
+        </Card.Header>
+        <br/>
+        <Card.Text>
+        <Form id="addDoctor" noValidate validated={validated} onSubmit={handleSubmit}>
+      <Row className="mb-3">
+        <Form.Group as={Col} md="4" controlId="validationCustom01">
+          <Form.Label>First name</Form.Label>
+          <Form.Control
+            required
+            type="text"
+            placeholder="First name"
+          />
+          <Form.Control.Feedback type="invalid">
+              Please choose a username.
+            </Form.Control.Feedback>
+          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group as={Col} md="4" controlId="validationCustom02">
+          <Form.Label>Last name</Form.Label>
+          <Form.Control
+            required
+            type="text"
+            placeholder="Last name"
+          />
+          <Form.Control.Feedback type="invalid">
+              Please choose a username.
+            </Form.Control.Feedback>
+          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group as={Col} md="4" controlId="validationCustomUsername">
+          <Form.Label>Username</Form.Label>
+          <InputGroup hasValidation>
+            <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
+            <Form.Control
+              type="text"
+              placeholder="Username"
+              aria-describedby="inputGroupPrepend"
+              required
+            />
+            <Form.Control.Feedback type="invalid">
+              Please choose a username.
+            </Form.Control.Feedback>
+          </InputGroup>
+        </Form.Group>
+      </Row>
+      <Row className="mb-3">
+        <Form.Group as={Col} md="6" controlId="validationCustom03">
+          <Form.Label>City</Form.Label>
+          <Form.Control type="text" placeholder="City" required />
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid city.
+          </Form.Control.Feedback>
+        </Form.Group>
+        
+        <Form.Group as={Col} controlId="formGridState">
+          <Form.Label>State/Union Territory</Form.Label>
+          <Form.Select defaultValue="Choose State/Union Territory" required>
+          <Form.Control required as="select" type="select"></Form.Control>
+          <option value="">Choose State...</option>
+          <option value="1">Andhra Pradesh</option>
+          <option value="2">Arunachal Pradesh</option>
+          <option value="3">Assam</option>
+          <option value="4">Bihar</option>
+          <option value="5">Chhattisgarh</option>
+          <option value="32">D&D</option>
+          <option value="6">Goa</option>
+          <option value="7">Gujarat</option>
+          <option value="8">Haryana</option>
+          <option value="9">Himachal Pradesh</option>
+          <option value="30">Jammu and Kashmir</option>
+          <option value="10">Jharkhand</option>
+          <option value="11">Karnataka</option>
+          <option value="12">Kerala</option>
+          <option value="31">Ladakh</option>
+          <option value="13">Madhya Pradesh</option>
+          <option value="14">Maharashtra</option>
+          <option value="15">Manipur</option>
+          <option value="16">Meghalaya</option>
+          <option value="17">Mizoram</option>
+          <option value="18">Nagaland</option>
+          <option value="19">Odisha</option>
+          <option value="20">Punjab</option>
+          <option value="21">Rajasthan</option>
+          <option value="22">Sikkim</option>
+          <option value="23">Tamil Nadu</option>
+          <option value="24">Telangana</option>
+          <option value="25">Tripura</option>
+          <option value="26">Uttar Pradesh</option>
+          <option value="27">Uttarakhand</option>
+          <option value="28">West Bengal</option>
+          <option value="29">Arunachal Pradesh</option>
+          </Form.Select>
+        </Form.Group>
+
+        <Form.Group as={Col} md="3" controlId="validationCustom05">
+          <Form.Label>Zip</Form.Label>
+          <Form.Control type="text" placeholder="Zip" required />
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid zip.
+          </Form.Control.Feedback>
+        </Form.Group>
+      </Row>
+
+      <Row className="mb-3">
+        <Form.Group as={Col} md="2" controlId="validationCustom06">
+          <Form.Label>Age</Form.Label>
+          <Form.Control type="Number" placeholder="Age" required />
+          <Form.Control.Feedback type="invalid">
+            Enter your Age
+          </Form.Control.Feedback>
+        </Form.Group>
+
+        <Form.Group as={Col} md="6" controlId="validationCustom03">
+          <Form.Label>Doctor Registration ID</Form.Label>
+          <Form.Control type="text" placeholder="Doctor Registration ID" required />
+          <Form.Control.Feedback type="invalid">
+            Enter Registration ID
+          </Form.Control.Feedback>
+        </Form.Group>
+
+        <Form.Group as={Col} md="4" controlId="validationCustom03">
+          <Form.Label>AADHAR Number</Form.Label>
+          <Form.Control type="text" placeholder="AADHAR Number" required />
+          <Form.Control.Feedback type="invalid">
+            Enter AADHAR NUMBER
+          </Form.Control.Feedback>
+        </Form.Group>
+
+      </Row>
+
+      <Row className="mb-3">
+        <Form.Group as={Col} md="4" controlId="formGridState">
+          <Form.Label>Specialization</Form.Label>
+          <Form.Select defaultValue="Choose Specialization" required>
+          <Form.Control required as="select" type="select" name="Specialization"></Form.Control>
+          <option value="">Choose Specialization</option>
+          <option value="1">Neurologist</option>
+          <option value="2">Cardiologist</option>
+          </Form.Select>
+        </Form.Group>
+
+        <Form.Group as={Col} md="4" controlId="formGridState">
+          <Form.Label>Designation</Form.Label>
+          <Form.Select defaultValue="Choose Designation" required>
+          <Form.Control required as="select" type="select" name="Designation"></Form.Control>
+          <option value="">Choose Designation</option>
+          <option value="1">Senior</option>
+          <option value="2">Junior</option>
+          <option value="3">Intern</option>
+          <option value="4">Guest</option>
+          </Form.Select>
+        </Form.Group>
+
+        <Form.Group as={Col} md="4" controlId="formGridState">
+          <Form.Label>CRED</Form.Label>
+          <Form.Select defaultValue="Choose CRED" required>
+          <Form.Control required as="select" type="select" name="CRED"></Form.Control>
+          <option value="">Choose CRED</option>
+          <option value="1">MD</option>
+          <option value="2">DO</option>
+          <option value="3">MBBS</option>
+          <option value="4">PhD</option>
+          </Form.Select>
+        </Form.Group>
+        
+      </Row>
+      <br/>
+      <Form.Group className="mb-3">
+        <Form.Check
+          required
+          label="Agree to terms and conditions"
+          feedback="You must agree before submitting."
+          feedbackType="invalid"
+        />
+      </Form.Group>
+      <Button type="submit">Submit form</Button>
+    </Form>
+        </Card.Text>
+        </Card.Body>
+        </Card>
+        </div>
         <Footer />
+        </div>
     </>
   )
 }
 
-export default hospital;
+export default Hospital;
--- src/components/Hospital/hospital.js
+++ src/components/Hospital/hospital.js
@@ -2,12 +2,17 @@ import React from 'react';
 import Navbar from './Navbar/navbar';
 import Footer from '../Footer/footer';
 
+
 function hospital() {
   return (
     <>
+        <div style={{'background': 'linear-gradient(#7F7FD5, #91eae4)' }}>
         <Navbar />
-        <div id='new'>hospital</div>
+        <div id="hospital" style={{'min-height':'50vh'}}>
+        Hospital
+        </div>
         <Footer />
+        </div>
     </>
   )
 }

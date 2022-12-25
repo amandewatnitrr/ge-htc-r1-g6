caca1d188323b811b288f0c64f8114d7e4f96d9f
Aman Kumar Dewangan <amandewatnitrr@gmail.com>
2022-12-25 17:53:01 +0530

    Add files via upload

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -6,6 +6,7 @@ import gif2 from '../imgs/4b229396885b90ea126258e5d19370ec.gif';
 import gif3 from '../imgs/c_hipertensao.gif';
 import Card from 'react-bootstrap/Card';
 import './css/home.css'
+import pic1 from '../imgs/62e8f3d81329f4d3d4b3f7dc_StateofAutomationBlueHeader.png';
 
 function doctor_app() {
   return (
@@ -51,56 +52,89 @@ function doctor_app() {
     </Card>
     </center>
     <br/>
-
-    
-      {
-      /**
-        * @Takchhya - These are the cards, do me a favour, just make sure when they are in desktop view, it's 2 cards on 1 row,
-        * while in mobile view, it's 1 card in 1 row. Work on it and make sure it's resolved.
-       **/
-
-      /* 
-      */
-      
-      }
     <center>
     <div id="row">
     <div id="column">
-    <Card id="homeservicefor">
+    <Card bg="dark" id="homeservicefor">
         <Card.Header><Card.Title><center>For Patients</center></Card.Title></Card.Header>
         <Card.Body>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
+          <Card.Text style={{'text-align':'left'}}>
+            <ul>
+              <li>Should be able to securely access their patient profile</li>
+              <li>Should be able to view all the digital prescriptions based on their visit to the hospitals</li>
+              <li>Should be able to view their medical test reports</li>
+              <li>Should be able to view their hospital visit history in a graphical view</li>
+            </ul>
           </Card.Text>
         </Card.Body>
       </Card>
     </div>
     <div id="column">
-      <Card id="homeservicefor"> 
+      <Card bg="dark" id="homeservicefor"> 
         <Card.Header><Card.Title><center>For Hospitals</center></Card.Title></Card.Header>
         <Card.Body>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
+          <Card.Text style={{'text-align':'left'}}>
+            <ul>
+              <li>Should be able to onboard new doctor/provider into the EHS system</li>
+              <li>Should be able to onboard new patients into the EHR system</li>
+              <li>Should be able to view all the patients onboarded</li>
+              <li>Should be able to view all the doctors/providers onboarded</li>
+              <li>Should be able to create digital prescriptions with patient and the doctor details</li>
+              <li>Should be able to upload medical test reports for a particular patient</li>
+            </ul>
           </Card.Text>
         </Card.Body>
       </Card>
     </div>
 
     <div id="column">
-      <Card id="homeservicefor"> 
+      <Card bg="dark" id="homeservicefor" style={{'text-align':'left'}}> 
         <Card.Header><Card.Title><center>For Doctors</center></Card.Title></Card.Header>
         <Card.Body>
           <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
+          <li>Should be able to securely access their work profile</li>
+          <li>Should be able to view all the digital prescriptions written by him/her</li>
+          <li>Should be able to view their consultation history in a graphical view</li>
           </Card.Text>
         </Card.Body>
       </Card>
     </div>
     </div>
     </center>
+    <br/>
+
+    <div id="row">
+      <div id="column">
+        <img id="homeimage1" alt="homeimage1" src={pic1} />
+      </div>
+
+      <div id="column">
+        <Card id="servicedetails">
+          <Card.Header id="AboutUs">
+            <h3><b>About Us</b></h3>
+          </Card.Header>
+          <Card.Body>
+            <Card.Text style={{'text-align': 'justify'}}>
+            <b>
+            For 40 years, we’ve worked at the intersection of healthcare and information technology to connect people and systems 
+            around the world. We use the latest technology to create solutions that let communities and people engage in their own 
+            health. Whether they are supporting the clinical, financial or operational areas of a hospital or health system, our 
+            tools are designed to work for today and think for tomorrow. 
+            <br/><br/><br/>
+            
+            <p>We support our clients by surfacing data that enables them 
+            to make informed decisions for better management of operations, while arming their clinicians with the information they
+            need to provide smarter care. Empowering them to know, manage and engage with the people they serve. All to disrupt the 
+            industry and transform the way healthcare is delivered. It’s our mission to relentlessly seek breakthrough innovation that 
+            will shape healthcare of tomorrow.
+            </p>
+            </b>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </div>
+    </div>
+
     </div>
     <br/>
     <Footer />
